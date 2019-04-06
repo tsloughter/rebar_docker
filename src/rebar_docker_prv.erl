7aaaca6140f77a04e90afc653a4670ac0edf0cbb
Tristan Sloughter <t@crashfast.com>
2019-04-06 17:44:06 -0600

    depend on lock provider

--- src/rebar_docker_prv.erl
+++ src/rebar_docker_prv.erl
@@ -3,7 +3,7 @@
 -export([init/1, do/1, format_error/1]).
 
 -define(PROVIDER, docker).
--define(DEPS, [app_discovery]).
+-define(DEPS, [lock]).
 
 -define(builder(ImageName, Checksum), [ImageName, "_builder:", Checksum]).
 -define(runner(ImageName, GitRef), [ImageName, ":", GitRef]).
